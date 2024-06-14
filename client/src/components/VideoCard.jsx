a7ee9763062fd1170cf632f76a433aaf8d66b55e
Debaditya <debadityabarman12@gmail.com>
2024-06-14 15:23:03 +0530

    2nd

--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -6,12 +6,17 @@ import { Box, Typography } from "@mui/material"
 const VideoCard = ({item}) => {
   console.log(item)
   return (
-    <Box sx={{width:'320px'}}>
-      <Typography>
-        
-      </Typography>
+    <Link to={`/video/${item.id.videoID}`}>
+    <Box sx={{width: '320px'}}>
       <img src={item.snippet.thumbnails.medium.url} alt="thumbnail" />
+      <Typography mt={1} mb={0.8} sx={{color:'#fff',fontWeight: 550, fontSize: 15}}>
+        {item.snippet.title <= 80 ? item.snippet.title : (item.snippet.title.substr(0,80)+"...")}
+      </Typography>
+      <Typography sx={{color:'#7F7F7F',fontWeight: 'light',fontSize: 15}}>
+        {item.snippet.channelTitle}
+      </Typography>
     </Box>
+    </Link>
   )
 }
 
--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -3,7 +3,7 @@ import VideoCard from "./VideoCard"
 
 const Videos = ({videos,view}) => {
   return(
-    <Box pl={view === true? '13vw' : '6vw'} sx={{display:'flex',flexWrap:'wrap',bgcolor:'#0F0F0F', width:'auto'}}>
+    <Box pl={view === true? '13vw' : '6vw'} sx={{display:'flex',flexWrap:'wrap',gap:view === true? '0.6rem' : '2rem',rowGap:'2rem',bgcolor:'#0F0F0F'}}>
       {videos.map((item,idx) => {
         return(
         <VideoCard item={item} key={idx} />
